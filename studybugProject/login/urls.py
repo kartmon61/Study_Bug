bfb1111afb20ef9ececa8bba9cfa8adba72b72f5
ji <alslahdk@naver.com>
2019-11-12 22:55:49 +0900

    ji

--- studybugProject/login/urls.py
+++ studybugProject/login/urls.py
@@ -3,5 +3,6 @@ from .import views
 
 urlpatterns = [
     #메인 페이지
-    path('',views.login,name='login'),
+    path('',views.loading,name='loading'),
+    path('login',views.login,name='login'),
 ]
--- studybugProject/login/views.py
+++ studybugProject/login/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def login(request):
     return render(request,'login.html')
+
+
+# 로딩 화면     
+def loading(request):
+    return render(request,'loading.html')

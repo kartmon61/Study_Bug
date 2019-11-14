0bc662966b7ada120d037f5c67020d02a4ac369f
ji <alslahdk@naver.com>
2019-11-15 04:20:56 +0900

    ji fe

--- studybugProject/main/views.py
+++ studybugProject/main/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.core.paginator import Paginator
+from post.models import Student, Comment
 
 # Create your views here.
 def main(request):
-    return render(request,'main.html')
+    posts = Student.objects.all().order_by('-id')
+    paginator = Paginator(posts,5)  
+    page_posts = paginator.get_page(1)
+
+    return render(request,'main.html',{'rlist':page_posts})

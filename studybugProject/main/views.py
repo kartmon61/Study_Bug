35113af8354e505e533b2c28a742cb1d7ba93c2f
ji <alslahdk@naver.com>
2019-11-16 00:03:50 +0900

    update

--- studybugProject/main/views.py
+++ studybugProject/main/views.py
@@ -8,5 +8,8 @@ def main(request):
     posts = Student.objects.all().order_by('-id')
     paginator = Paginator(posts,5)  
     page_posts = paginator.get_page(1)
-
+    category_no = request.GET.get('category_no')
+    if category_no is not None:
+        redirect('post')
     return render(request,'main.html',{'rlist':page_posts})
+ 
